189a9b061fad0bd9009b6f3535eab3709341d26c
darmijo15 <darmijo15@gmail.com>
2022-08-31 17:20:21 -0700

    edit design of webpage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { 
     provideFASTDesignSystem, 
+    fastAnchor, 
     fastButton
   } from '@microsoft/fast-components';
   
   provideFASTDesignSystem()
       .register(
+          fastAnchor(),
           fastButton()
       );
 import './style.css';
-
-console.log('test');
