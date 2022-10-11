6580d1c9b12713733198a67b97164a92d40548dd
darmijo15 <darmijo15@gmail.com>
2022-10-10 21:36:40 -0700

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                exlude: /node_modules/,
+                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
