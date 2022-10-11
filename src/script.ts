7ebb133a25dc02e478bd694ba178f38414119380
darmijo15 <darmijo15@gmail.com>
2022-10-10 21:39:33 -0700

    replace index.js with script.ts as the entry point

---
+++ src/script.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+const mySearch = document.querySelector('my-search');
+
+mySearch?.addEventListener('find', e => console.log((<CustomEvent>e).detail))
+
+export * from './components/my-search'
+export * from './components/pre-built'
