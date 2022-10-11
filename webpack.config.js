9010ad7d75ee31ecb71ad94137095c57a13f9aff
darmijo15 <darmijo15@gmail.com>
2022-10-10 20:56:32 -0700

    use one config file for Webpack, set production mode using scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,53 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: './src/script.ts',
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'src'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ]
-            }
-        ]
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            {
+                test: /\.js$/,
+                exlude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/template.html',
-            filename: 'index.html',
-            title: 'FAST Project',
-            minify: { collapseWhitespace: false },
-        }),
-    ],
-}
+};
