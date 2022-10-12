5ddde76b617a8784830d00bd37ec98331dc318fa
darmijo15 <darmijo15@gmail.com>
2022-10-11 22:57:23 -0700

    export the component and use it in the html file

--- src/script.ts
+++ src/script.ts
@@ -6,3 +6,4 @@ mySearch?.addEventListener('find', e => console.log((<CustomEvent>e).detail))
 
 export * from './components/my-search'
 export * from './components/pre-built'
+export * from './components/user-card'
